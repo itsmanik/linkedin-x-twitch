5e296055558983d348bce30e4d37e64decb3ba41
itsmanik <manik.hilalpure@gmail.com>
2024-05-22 01:19:56 +0530

    added jobs section

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,5 +1,13 @@
+import Job from "./Job"
+
 const Home = () => {
-    return <div className="text-white w-full mt-16 h-[90vh] overflow-y-scroll overflow-x-hidden p-8">Jobs</div>;
+    return (
+        <div className="text-white w-full mt-16 h-[90vh] overflow-y-scroll overflow-x-hidden p-8">
+            <h1 className="text-xl font-bold">Explore opportunities</h1>
+            <p className="text-gray-500 text-sm">Based on your profile and search history</p>
+            <Job />
+        </div>
+    );
 };
 
 export default Home;
